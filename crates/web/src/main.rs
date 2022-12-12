aa47f3854d9f433c096879d42413dfcbef62b93b
CarloMicieli <carlo.micieli@gmail.com>
2022-12-12 08:24:06 +0100

    test(catalog): add the integration test for the health check endpoint

--- crates/web/src/main.rs
+++ crates/web/src/main.rs
@@ -1,23 +1,8 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::net::TcpListener;
+use web::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
-}
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok().body("OK")
-}
-
-#[cfg(test)]
-mod test {
-    use pretty_assertions::assert_eq;
-
-    #[test]
-    fn it_should_always_pass_web() {
-        assert_eq!(21 * 2, 42);
-    }
+    let listener = TcpListener::bind("127.0.0.1:8080").expect("Failed to bind port");
+    run(listener)?.await
 }
---
+++ crates/web/tests/health_check.rs
@@ -0,0 +1,20 @@
+mod common;
+
+use crate::common::spawn_app;
+use pretty_assertions::assert_eq;
+
+#[tokio::test]
+async fn health_check_works() {
+    let address = spawn_app();
+
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get(&format!("{}/health_check", &address))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
