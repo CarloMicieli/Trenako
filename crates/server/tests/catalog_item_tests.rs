3d2a4dc6a72b7182802d7e1b2a2ad12abc990f4e
CarloMicieli <carlo.micieli@gmail.com>
2023-01-26 07:39:53 +0100

    test(catalog): ensure the catalog item is actually saved in the database

--- crates/server/tests/catalog_item_tests.rs
+++ crates/server/tests/catalog_item_tests.rs
@@ -1,6 +1,14 @@
 use crate::common::seeding::{seed_brands, seed_scales};
 use crate::common::{create_docker_test, spawn_app, IMAGE_NAME};
+use catalog::brands::brand_id::BrandId;
+use catalog::catalog_items::availability_status::AvailabilityStatus;
+use catalog::catalog_items::catalog_item_id::CatalogItemId;
+use catalog::catalog_items::category::Category;
+use catalog::catalog_items::delivery_date::DeliveryDate;
+use catalog::catalog_items::power_method::PowerMethod;
+use catalog::scales::scale_id::ScaleId;
 use serde_json::json;
+use std::str::FromStr;
 
 pub mod common;
 
@@ -20,7 +28,8 @@ async fn post_new_catalog_items() {
         seed_brands(&pg_pool).await;
         seed_scales(&pg_pool).await;
 
-        let expected_location = format!("{}/{}", API_CATALOG_ITEMS, "acme-123456");
+        let catalog_item_id = CatalogItemId::from_str("acme-123456").unwrap();
+        let expected_location = format!("{}/{}", API_CATALOG_ITEMS, catalog_item_id);
 
         let request = json!({
             "brand" : "ACME",
@@ -52,6 +61,54 @@ async fn post_new_catalog_items() {
         assert!(response.status().is_success());
         assert_eq!(Some(0), response.content_length());
         assert_eq!(expected_location, response.headers()["Location"]);
+
+        let saved = sqlx::query_as!(
+            Saved,
+            r#"SELECT
+                    item_number,
+                    brand_id as "brand_id: BrandId",  scale_id as "scale_id: ScaleId", 
+                    category as "category: Category",
+                    power_method as "power_method: PowerMethod",
+                    description_it, details_it,
+                    delivery_date, 
+                    availability_status as "availability_status: AvailabilityStatus", 
+                    count
+                FROM catalog_items WHERE catalog_item_id = $1"#,
+            catalog_item_id as CatalogItemId
+        )
+        .fetch_one(&pg_pool)
+        .await
+        .expect("Failed to fetch saved catalog item.");
+
+        assert_eq!(BrandId::new("ACME"), saved.brand_id);
+        assert_eq!("123456", saved.item_number);
+        assert_eq!(Category::Locomotives, saved.category);
+        assert_eq!(ScaleId::new("H0"), saved.scale_id);
+        assert_eq!(PowerMethod::DC, saved.power_method);
+        assert_eq!(
+            Some(String::from(
+                "Locomotiva elettrica E 402A 015 nella livrea di origine rosso/bianco, pantografi 52 Sommerfeldt"
+            )),
+            saved.description_it
+        );
+        assert_eq!(Some(String::from("Motore a 5 poli")), saved.details_it);
+        assert_eq!(Some(DeliveryDate::by_year(2005).to_string()), saved.delivery_date);
+        assert_eq!(Some(AvailabilityStatus::Available), saved.availability_status);
+        assert_eq!(1, saved.count);
     })
     .await
+}
+
+#[derive(Debug)]
+struct Saved {
+    brand_id: BrandId,
+    item_number: String,
+    category: Category,
+    scale_id: ScaleId,
+    power_method: PowerMethod,
+    description_it: Option<String>,
+    details_it: Option<String>,
+    delivery_date: Option<String>,
+    availability_status: Option<AvailabilityStatus>,
+    count: i32,
 }
