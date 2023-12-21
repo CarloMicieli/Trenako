e9d5cbe5e1bfe1a762394cc4e70bcf35ff684ec4
CarloMicieli <carlo.micieli@gmail.com>
2023-12-21 08:54:41 +0100

    refactor: add environment filter to tracing subscribers

--- crates/services/server/src/main.rs
+++ crates/services/server/src/main.rs
@@ -1,12 +1,15 @@
 use configuration::Settings;
 use server::app;
 use tokio::net::TcpListener;
+use tracing_subscriber::EnvFilter;
 
 #[tokio::main]
 async fn main() {
     let subscriber = tracing_subscriber::fmt()
         .with_target(false)
-        .with_thread_ids(true)
+        .with_thread_ids(false)
+        .with_thread_names(true)
+        .with_env_filter(EnvFilter::from_default_env())
         .json()
         .finish();
     tracing::subscriber::set_global_default(subscriber).unwrap();
@@ -19,7 +22,6 @@ async fn main() {
     println!("{}", &BANNER_TEXT);
     tracing::info!("Starting the server...");
     tracing::info!("{}", serde_json::to_string(&settings).unwrap());
-
     app::run(listener, &settings).await;
 }
 
